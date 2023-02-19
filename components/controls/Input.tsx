0843db3443e64b4db70c80968b00890a80753271
ejbaek <909back@naver.com>
2023-02-19 16:36:05 +0900

    [23/02/19] 909back * Input 컴포넌트: 작업중..

--- components/controls/Input.tsx
+++ components/controls/Input.tsx
@@ -1,19 +1,27 @@
+import React, { InputHTMLAttributes } from 'react'
 import classNames from 'classnames/bind'
 import styles from '../../styles/Input.module.scss'
+import IconClose from '@/components/icons/ic-close'
 
 const cx = classNames.bind(styles)
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     children?: React.ReactNode
 }
 
 const Input = ({
-
+    className,
+    type='text',
+    value,
+    max = 6,
+    ...props
 }: InputProps) => {
     return (
-        <>
-            
-        </>
+        <label className={cx('input-container', className)}>
+            <input className={cx('input')} value={value} type={type} {...props}/>
+            {value && <button className={cx('close-button')}><IconClose/></button>}
+            <span className={cx('input-counter')}>{`${value?.toString().length??0}/${max}`}</span>
+        </label>
     )
 }
 
--- pages/setting/index.tsx
+++ pages/setting/index.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React,{useState} from "react"
 import styles from "../../styles/Setting.module.scss"
 import classNames from "classnames/bind"
 import PageLayout from "@/components/layout/PageLayout"
 import Progress from "@/components/controls/Progress"
+import Input from "@/components/controls/Input"
 
 const cx = classNames.bind(styles)
 
@@ -10,11 +11,15 @@ interface Settingprops {
   children?: React.ReactNode
 }
 const Setting = ({}:Settingprops) => {
+  const [nickname, setNickname] = useState<string>()
+
+  const handleChangeNickName = (e:React.ChangeEvent<HTMLInputElement>) => setNickname(e.target.value)
   return (
     <PageLayout>
       <div className={cx('setting-page')}>
         <Progress/>
         <p className={cx('page-desc')}>크루원의 새로운 이름을 정해주세요.</p>
+        <Input placeholder="닉네임을 입력해주세요" value={nickname} onChange={handleChangeNickName}/>
       </div>
     </PageLayout>
   )

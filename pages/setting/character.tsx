96934bf28afe7930bdf4a3213f36acb17c3468f5
Eunji Baek <91578165+909back@users.noreply.github.com>
2023-02-21 22:58:01 +0900

    [2023/02/21] 909back * 캐릭터 선택 페이지 수정

--- pages/setting/character.tsx
+++ pages/setting/character.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { CSSProperties, useEffect, useRef, useState } from "react"
 import { useRouter } from "next/router"
 import styles from "../../styles/SettingCharacter.module.scss"
 import classNames from "classnames/bind"
@@ -13,6 +13,14 @@ import Nely from "@/components/icons/character/nely.png"
 import { StaticImageData } from "next/image"
 
 const cx = classNames.bind(styles)
+
+export const TIMINGFUNC_MAP = {
+  linear: (t: number) => t,
+  'ease-in': (t: number) => t * t,
+  'ease-out': (t: number) => t * (2 - t),
+  'ease-in-out': (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+}
+
 
 interface characterItem<T extends string | number> {
   value: T,
@@ -29,22 +37,91 @@ const characterList: characterItem<number>[] = [
 
 interface CharacterSliderProps {
   data: characterItem<number>[],
-  selected?: number,
   onSelect?: (value: number) => void
 }
-const CharacterSlider = ({ data, selected, onSelect: handleSelect = () => { } }: CharacterSliderProps) => {
+const CharacterSlider = ({ data, onSelect: handleSelect = () => { } }: CharacterSliderProps) => {
+  const [index, setIndex] = useState<number>(0)
+  const [translate, setTranslate] = useState(0)
+  const touchX = useRef<number>(0)
+  const timing = TIMINGFUNC_MAP['ease-in-out']
+
+  useEffect(()=> {
+    handleSelect(data[index].value)
+  },[index])
+
+  const handleTouchStart = (e:React.TouchEvent) => {
+    if(index === data.length-1) return
+    touchX.current = e.touches[0].pageX
+  }
+
+  const handleTouchEnd = (e:React.TouchEvent) => {
+    const diff =  e.changedTouches[0].pageX - touchX.current
+
+    if(diff < 0) forward()
+    else back()
+    touchX.current = 0;
+  }
+
+  const forward = (duration= 400) => {
+    if(index > data.length-1) return
+
+    let start:number;
+
+    const step:FrameRequestCallback = (timestamp) => {
+      if(!start) start = timestamp;
+      const diff = timestamp - start
+      const progress = Math.min(1, diff/duration)
+
+      setTranslate(-1 * ((timing(progress) * 450) + (450 * index)) )
+      
+      if(diff < duration) requestAnimationFrame(step)
+      else endTransition()
+    }
+    
+    const id = requestAnimationFrame(step)
+    
+    const endTransition = () => {
+      cancelAnimationFrame(id)
+      setIndex(prev => prev+1)
+    }
+  }
+
+  const back = (duration= 400) => {
+    if(!index) return
+    let start:number;
+
+    const step:FrameRequestCallback = (timestamp) => {
+      if(!start) start = timestamp;
+      const diff = timestamp - start
+      const progress = Math.min(1, diff/duration)
+
+      setTranslate((timing(progress) * 450) - (450 * index))
+      
+      if(diff < duration) requestAnimationFrame(step)
+      else endTransition()
+    }
+    
+    const id = requestAnimationFrame(step)
+    
+    const endTransition = () => {
+      setIndex(prev => prev-1)
+      cancelAnimationFrame(id)
+    }
+  }
+
+
   return <div className={cx("character-slider-container")}>
-    <div className={cx('slider')}>
+    <div className={cx('slider')} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
       <div className={cx('slider-container')}>
-        <div className={cx('slider-wrapper')}>
-          {data.map((item, index) => <div key={item.name} className={cx('slider-item')}>
+        <div style={{transform: `translate3d(${translate}px,0,0)`}} className={cx('slider-wrapper')}>
+          {data.map(item => <div key={item.name} className={cx('slider-item')}>
             <Image src={item.src} alt={item.name} />
           </div>)}
         </div>
       </div>
     </div>
     <div className={cx('slider-index-container')}>
-      {data.map(item => <div key={item.name} className={cx('slide-index', { active: item.value === selected })} />)}
+      {data.map((item,idx) => <div key={item.name} className={cx('slide-index', { active: index === idx })} />)}
     </div>
   </div>
 }
@@ -57,6 +134,7 @@ const Character = ({ }: Characterprops) => {
   const { name: nickname } = router.query
   const [character, setCharacter] = useState(characterList[0].value)
 
+  console.log(character)
 
   return (
     <PageLayout>
@@ -64,7 +142,7 @@ const Character = ({ }: Characterprops) => {
         <section className={cx('select-character-section')}>
           <Progress className={cx('select-character-progress')} step={2} />
           <p className={cx('page-desc')}>변장시킬 크루원을 선택해주세요!</p>
-          <CharacterSlider data={characterList} selected={character} onSelect={setCharacter} />
+          <CharacterSlider data={characterList} onSelect={setCharacter} />
         </section>
         <Button>선택</Button>
       </section>

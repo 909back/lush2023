76866117c24de32d987cd1476b4c5b0defb714d2
Eunji Baek <91578165+909back@users.noreply.github.com>
2023-03-02 22:56:10 +0900

    [2023/03/02] 909back * api 수정'

--- pages/api/v1/character/index.ts
+++ pages/api/v1/character/index.ts
@@ -9,7 +9,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { method } = req
     if (method === 'POST') {
         const { id, name, head, body, face, head_width, head_y, body_width, body_y, face_width, face_y } = req.body
-        console.log(id.isNumber())
+
         if (!id || !id.isNumber()) res.status(400).send({ code: 400, message: '아이디(숫자)가 없습니다.' })
         if (!name) res.status(400).send({ message: '이름이 없습니다.' })
         if (!head || !body || !face) res.status(400).send({ code: 400, message: 'head,body,face는 필수값 입니다.' })
@@ -18,8 +18,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         const result: any = await mariadb('INSERT INTO characters(id, name, head, head_w, head_y, body, body_w, body_y, face, face_w, face_y) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [parseInt(id), name, head, parseInt(head_width), parseInt(head_y), body, parseInt(body_width), parseInt(body_y), face, parseInt(face_width), parseInt(face_y)]);
         await addLog(req)
-        if (result.affectedRows) res.status(200).send(true)
-        else throw { code: 500, message: '' }
+        if (result.affectedRows) return res.status(200).send(true)
+        else throw { code: 500, message: '실패했습니다.' }
     }
 }
 

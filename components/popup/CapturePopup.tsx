7afe34e733407aadbc6381e77d723f2c1c64d406
909back <909back@naver.com>
2023-03-06 14:01:02 +0900

    [23/03/06] 909back * 레이아웃 수정"

--- components/popup/CapturePopup.tsx
+++ components/popup/CapturePopup.tsx
@@ -1,47 +1,42 @@
-import { useRouter } from 'next/router'
-import classNames from 'classnames/bind'
-import styles from '../../styles/CapturePopup.module.scss'
-import Button from '../controls/Button'
-import { PopupTypes } from '@/interfaces'
-import { useRecoilState } from 'recoil'
-import { userImage } from '@/recoil/atom'
+import {useRouter} from "next/router";
+import classNames from "classnames/bind";
+import styles from "../../styles/CapturePopup.module.scss";
+import Button from "../controls/Button";
+import {PopupTypes} from "@/interfaces";
+import {useRecoilState} from "recoil";
+import {userImage} from "@/recoil/atom";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
-interface CapturePopupProps extends PopupTypes.Default{
-    children?: React.ReactNode
-    image: string
-
+interface CapturePopupProps extends PopupTypes.Default {
+  children?: React.ReactNode;
+  image: string;
 }
 
-const CapturePopup = ({
- onPositive:handlePositive = () =>{},
- onNegative: handleNegative = () => {},
- image,
-}: CapturePopupProps) => {
-    const [_,setImage] = useRecoilState(userImage)
-    const router = useRouter()
-    const handleClick = () => {
-        handlePositive()
-        sessionStorage.setItem('image', image)
-        setImage(image)
-        router.push('/print')
-    }
-    return (    
-        <div className={cx('capture-container')}>
-            <section className={cx('capture-section')}>
-                <img src={image} alt="완성된 그림" className={cx('complete-png')}/>
-                <section className={cx('carmera-section')}>
-                    <div className={cx('rect-top-left')}/>
-                    <div className={cx('rect-top-right')}/>
-                    <div className={cx('rect-center')}/>
-                    <div className={cx('rect-bottom-left')}/>
-                    <div className={cx('rect-bottom-right')}/>
-                </section>
-            </section>
-            <Button onClick={handleClick} >촬영하기</Button>
-        </div>
-    )
-}
+const CapturePopup = ({onPositive: handlePositive = () => {}, onNegative: handleNegative = () => {}, image}: CapturePopupProps) => {
+  const [_, setImage] = useRecoilState(userImage);
+  const router = useRouter();
+  const handleClick = () => {
+    handlePositive();
+    sessionStorage.setItem("image", image);
+    setImage(image);
+    router.push("/print");
+  };
+  return (
+    <div className={cx("capture-container")}>
+      <section className={cx("capture-section")}>
+        <img src={image} alt="완성된 그림" className={cx("complete-png")} />
+        <section className={cx("carmera-section")}>
+          <div className={cx("rect-top-left")} />
+          <div className={cx("rect-top-right")} />
+          <div className={cx("rect-center")} />
+          <div className={cx("rect-bottom-left")} />
+          <div className={cx("rect-bottom-right")} />
+        </section>
+      </section>
+      <Button onClick={handleClick}>촬영하기</Button>
+    </div>
+  );
+};
 
-export default CapturePopup
+export default CapturePopup;
--- pages/setting/character.tsx
+++ pages/setting/character.tsx
@@ -1,163 +1,169 @@
-import React, { CSSProperties, MemoExoticComponent, useEffect, useRef, useState } from "react"
-import { useRouter } from "next/router"
-import styles from "../../styles/SettingCharacter.module.scss"
-import classNames from "classnames/bind"
-import Image from "next/image"
-import PageLayout from "@/components/layout/PageLayout"
-import Progress from "@/components/controls/Progress"
-import Button from "@/components/controls/Button"
-import Hippy from "@/components/icons/character/hippy"
-import Luky from "@/components/icons/character/luky"
-import Star from "@/components/icons/character/star"
-import Nely from "@/components/icons/character/nely"
-import { login } from "@/utils/apiHook"
-import useToast from "@/utils/useToast"
-import { CharType } from "@/interfaces"
+import React, {CSSProperties, MemoExoticComponent, useEffect, useRef, useState} from "react";
+import {useRouter} from "next/router";
+import styles from "../../styles/SettingCharacter.module.scss";
+import classNames from "classnames/bind";
+import Image from "next/image";
+import PageLayout from "@/components/layout/PageLayout";
+import Progress from "@/components/controls/Progress";
+import Button from "@/components/controls/Button";
+import Hippy from "@/components/icons/character/hippy";
+import Luky from "@/components/icons/character/luky";
+import Star from "@/components/icons/character/star";
+import Nely from "@/components/icons/character/nely";
+import {login} from "@/utils/apiHook";
+import useToast from "@/utils/useToast";
+import {CharType} from "@/interfaces";
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 export const TIMINGFUNC_MAP = {
   linear: (t: number) => t,
-  'ease-in': (t: number) => t * t,
-  'ease-out': (t: number) => t * (2 - t),
-  'ease-in-out': (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
-}
-
+  "ease-in": (t: number) => t * t,
+  "ease-out": (t: number) => t * (2 - t),
+  "ease-in-out": (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+};
 
 interface characterItem<T extends any> {
-  value: T,
-  name: string,
-  icon: MemoExoticComponent<any>
+  value: T;
+  name: string;
+  icon: MemoExoticComponent<any>;
 }
 
 const characterList: characterItem<string>[] = [
-  { value: 'nely', name: 'nely', icon: Nely },
-  { value: 'luky', name: 'luky', icon: Luky },
-  { value: 'star', name: 'star', icon: Star },
-  { value: 'hippy', name: 'hippy', icon: Hippy },
-]
+  {value: "nely", name: "nely", icon: Nely},
+  {value: "luky", name: "luky", icon: Luky},
+  {value: "star", name: "star", icon: Star},
+  {value: "hippy", name: "hippy", icon: Hippy},
+];
 
 interface CharacterSliderProps<T> {
-  data: characterItem<T>[],
-  onSelect?: (value: T) => void
+  data: characterItem<T>[];
+  onSelect?: (value: T) => void;
 }
-const CharacterSlider = <T extends any>({ data, onSelect: handleSelect = () => { } }: CharacterSliderProps<T>) => {
-  const [index, setIndex] = useState<number>(0)
-  const [translate, setTranslate] = useState(0)
-  const touchX = useRef<number>(0)
-  const timing = TIMINGFUNC_MAP['ease-in-out']
+const CharacterSlider = <T extends any>({data, onSelect: handleSelect = () => {}}: CharacterSliderProps<T>) => {
+  const [index, setIndex] = useState<number>(0);
+  const [translate, setTranslate] = useState(0);
+  const touchX = useRef<number>(0);
+  const timing = TIMINGFUNC_MAP["ease-in-out"];
 
   useEffect(() => {
-    handleSelect(data[index].value)
-  }, [index])
+    handleSelect(data[index].value);
+  }, [index]);
 
   const handleTouchStart = (e: React.TouchEvent) => {
-    if (index === data.length - 1) return
-    touchX.current = e.touches[0].pageX
-  }
+    document.body.style.overflowX = "hidden";
+    if (index === data.length - 1) return;
+    touchX.current = e.touches[0].pageX;
+  };
 
   const handleTouchEnd = (e: React.TouchEvent) => {
-    const diff = e.changedTouches[0].pageX - touchX.current
+    const diff = e.changedTouches[0].pageX - touchX.current;
 
-    if (diff < 0) forward()
-    else back()
+    if (diff < 0) forward();
+    else back();
     touchX.current = 0;
-  }
+    document.body.style.overflowX = "";
+  };
 
   const forward = (duration = 400) => {
-    if (index >= data.length - 1) return
+    if (index >= data.length - 1) return;
 
     let start: number;
 
     const step: FrameRequestCallback = (timestamp) => {
       if (!start) start = timestamp;
-      const diff = timestamp - start
-      const progress = Math.min(1, diff / duration)
+      const diff = timestamp - start;
+      const progress = Math.min(1, diff / duration);
 
-      setTranslate(-1 * ((timing(progress) * 450) + (450 * index)))
+      setTranslate(-1 * (timing(progress) * 450 + 450 * index));
 
-      if (diff < duration) requestAnimationFrame(step)
-      else endTransition()
-    }
+      if (diff < duration) requestAnimationFrame(step);
+      else endTransition();
+    };
 
-    const id = requestAnimationFrame(step)
+    const id = requestAnimationFrame(step);
 
     const endTransition = () => {
-      cancelAnimationFrame(id)
-      setIndex(prev => prev + 1)
-    }
-  }
+      cancelAnimationFrame(id);
+      setIndex((prev) => prev + 1);
+    };
+  };
 
   const back = (duration = 400) => {
-    if (!index) return
+    if (!index) return;
     let start: number;
 
     const step: FrameRequestCallback = (timestamp) => {
       if (!start) start = timestamp;
-      const diff = timestamp - start
-      const progress = Math.min(1, diff / duration)
+      const diff = timestamp - start;
+      const progress = Math.min(1, diff / duration);
 
-      setTranslate((timing(progress) * 450) - (450 * index))
+      setTranslate(timing(progress) * 450 - 450 * index);
 
-      if (diff < duration) requestAnimationFrame(step)
-      else endTransition()
-    }
+      if (diff < duration) requestAnimationFrame(step);
+      else endTransition();
+    };
 
-    const id = requestAnimationFrame(step)
+    const id = requestAnimationFrame(step);
 
     const endTransition = () => {
-      setIndex(prev => prev - 1)
-      cancelAnimationFrame(id)
-    }
-  }
+      setIndex((prev) => prev - 1);
+      cancelAnimationFrame(id);
+    };
+  };
 
-
-  return <div className={cx("character-slider-container")}>
-    <div className={cx('slider')} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
-      <div className={cx('slider-container')}>
-        <div style={{ transform: `translate3d(${translate}px,0,0)` }} className={cx('slider-wrapper')}>
-          {data.map(({ name, icon: Icon }) => <div key={name} className={cx('slider-item')}>
-            <Icon />
-          </div>)}
+  return (
+    <div className={cx("character-slider-container")}>
+      <div className={cx("slider")} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
+        <div className={cx("slider-container")}>
+          <div style={{transform: `translate3d(${translate}px,0,0)`}} className={cx("slider-wrapper")}>
+            {data.map(({name, icon: Icon}) => (
+              <div key={name} className={cx("slider-item")}>
+                <Icon />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+      <div className={cx("slider-index-container")}>
+        {data.map((item, idx) => (
+          <div key={item.name} className={cx("slide-index", {active: index === idx})} />
+        ))}
+      </div>
     </div>
-    <div className={cx('slider-index-container')}>
-      {data.map((item, idx) => <div key={item.name} className={cx('slide-index', { active: index === idx })} />)}
-    </div>
-  </div>
-}
+  );
+};
 
 interface Characterprops {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
-const Character = ({ }: Characterprops) => {
-  const router = useRouter()
-  const { name: nickname } = router.query
-  const [character, setCharacter] = useState(characterList[0].value)
-  const { addToast } = useToast()
+const Character = ({}: Characterprops) => {
+  const router = useRouter();
+  const {name: nickname} = router.query;
+  const [character, setCharacter] = useState(characterList[0].value);
+  const {addToast} = useToast();
 
   const handleClickComplete = async (e: React.MouseEvent) => {
     try {
-      await login(nickname as string, character as keyof typeof CharType.List)
-      router.push(`/custom/${character}`,`/custom/${character}`,{shallow:true})
+      await login(nickname as string, character as keyof typeof CharType.List);
+      router.push(`/custom/${character}`, `/custom/${character}`, {shallow: true});
     } catch (err) {
-      if (err) addToast(err as string)
+      if (err) addToast(err as string);
     }
-  }
-  
+  };
+
   return (
     <PageLayout>
-      <section className={cx('setting-character')}>
-        <section className={cx('select-character-section')}>
-          <Progress className={cx('select-character-progress')} step={2} />
-          <p className={cx('page-desc')}>변장시킬 크루원을 선택해주세요!</p>
+      <section className={cx("setting-character")}>
+        <section className={cx("select-character-section")}>
+          <Progress className={cx("select-character-progress")} step={2} />
+          <p className={cx("page-desc")}>변장시킬 크루원을 선택해주세요!</p>
           <CharacterSlider data={characterList} onSelect={setCharacter} />
         </section>
         <Button onClick={handleClickComplete}>선택</Button>
       </section>
     </PageLayout>
-  )
-}
+  );
+};
 
-export default Character
+export default Character;
